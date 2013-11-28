5e77a48bbb3415558ab4be2ae3798a37d2ec4e6a
Zhao Li <zhaol@hawaii.edu>
2013-11-27 23:02:11 -0500

    update bootstrap.rb

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,5 +1,13 @@
 !# /usr/bin/env ruby
 
-puts "hi"
+# Usage: curl https://ee160-c9-zhaol.c9.io/code/bootstrap.rb | ruby
+puts "bootstrapping your workspace for EE160 (this might take awhile)..."
+puts "installing dependencies..."
 `gem install twitter -v 5.0.0.rc.1`
-puts "bye"
+`gem install wavefile`
+puts "setting up your scripts"
+Dir.chdir do
+  `touch hi.bye`
+  `git clone https://github.com/zhaol/ee160_scripts.git`
+end
+puts "your workspace is now bootstrapped...have a wonderful semester"
