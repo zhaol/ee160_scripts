b8f940a557e586b2f5995d3d059c9990fc52d2d3
Zhao Li <zhaol@hawaii.edu>
2013-11-30 01:51:06 -0500

    add folder scaffold to bootstrap

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,13 +1,17 @@
 !# /usr/bin/env ruby
 
-# Usage: curl https://ee160-c9-zhaol.c9.io/code/bootstrap.rb | ruby
 puts "bootstrapping your workspace for EE160 (this might take awhile)..."
+
+puts "setting up your directories (i.e. folders)"
+  `git clone https://github.com/zhaol/ee160_scaffold.git`
+
 puts "installing dependencies..."
-`gem install twitter -v 5.0.0.rc.1`
-`gem install wavefile`
+  `gem install twitter -v 5.0.0.rc.1`
+  `gem install wavefile`
+
 puts "setting up your scripts"
 Dir.chdir do
-  `touch hi.bye`
   `git clone https://github.com/zhaol/ee160_scripts.git`
 end
+
 puts "your workspace is now bootstrapped...have a wonderful semester"
