27ee6684f11f683350f64f752c179f51c308d4a4
Zhao Li <zhaol@hawaii.edu>
2014-09-03 10:07:03 +0000

    move bootstrap gems to Gemfile

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'twitter', '5.11.0'
+gem 'wavefile', '0.6.0'
+gem 'thor', '0.19.1'
+gem 'numbers_and_words', '0.10.0'
--- bootstrap.rb
+++ bootstrap.rb
@@ -6,12 +6,8 @@ puts "setting up your directories (i.e. folders)"
   `git clone https://github.com/zhaol/ee160_scaffold.git ee160`
 
 puts "installing dependencies..."
-  `gem install twitter -v 5.11.0`
-  `gem install wavefile -v 0.6.0`
-  `gem install thor -v 0.19.1`
-  `gem install activesupport -v 4.1.5`
-  `gem install numbers_in_words -v 0.2.0`
-  `gem install numbers_and_words -v 0.10.0`
+  `gem install bundler -v 1.6.2`
+  `bundle`
 
 puts "setting up your scripts"
 Dir.chdir do
