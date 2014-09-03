aa5288d51a0179f63bd1a771cba8564d6b595eed
Zhao Li <zhaol@hawaii.edu>
2014-09-03 10:09:24 +0000

    move bundle command

--- bootstrap.rb
+++ bootstrap.rb
@@ -7,11 +7,11 @@ puts "setting up your directories (i.e. folders)"
 
 puts "installing dependencies..."
   `gem install bundler -v 1.6.2`
-  `bundle`
 
 puts "setting up your scripts"
 Dir.chdir do
   `git clone https://github.com/zhaol/ee160_scripts.git`
+  `cd ee160_scripts && bundle`
 end
 
 puts "your workspace is now bootstrapped...have a wonderful semester"
