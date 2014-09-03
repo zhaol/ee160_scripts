8d72d1f6db0525c967c071ba819edc60f924ffd5
Zhao Li <zhaol@hawaii.edu>
2014-09-03 10:00:47 +0000

    move Grader::Submission::Parser out to its own file

--- grader/submission.rb
+++ grader/submission.rb
@@ -41,32 +41,4 @@ class Grader::Submission
   end
 end
 
-class Grader::Submission::Parser
-  def initialize(assignment)
-    @assignment = assignment
-  end
-  
-  def major
-    @major || get_major_assignment_number
-  end
-  
-  def minor
-    @minor || get_minor_assignment_number
-  end
-  
-  private
-  
-  def get_major_assignment_number
-    parse_assignment_number
-    @major
-  end
-  
-  def parse_assignment_number
-    @major, @minor = @assignment.match(/([\d]+)[_]([\d]+)/).captures
-  end
-  
-  def get_minor_assignment_number
-    parse_assignment_number
-    @minor
-  end
-end
+require_relative 'submission/parser'
---
+++ grader/submission/parser.rb
@@ -0,0 +1,29 @@
+class Grader::Submission::Parser
+  def initialize(assignment)
+    @assignment = assignment
+  end
+  
+  def major
+    @major || get_major_assignment_number
+  end
+  
+  def minor
+    @minor || get_minor_assignment_number
+  end
+  
+  private
+  
+  def get_major_assignment_number
+    parse_assignment_number
+    @major
+  end
+  
+  def parse_assignment_number
+    @major, @minor = @assignment.match(/([\d]+)[_]([\d]+)/).captures
+  end
+  
+  def get_minor_assignment_number
+    parse_assignment_number
+    @minor
+  end
+end
