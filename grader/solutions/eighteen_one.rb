a97cf917a2203cb03ddab8faa79de75632fead2e
Zhao Li <zhaol@hawaii.edu>
2014-10-12 16:12:55 +0000

    updating 18_1 solution checking to account for comments left in the main function

--- grader/solutions/eighteen_one.rb
+++ grader/solutions/eighteen_one.rb
@@ -15,7 +15,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   private
   
   def verify_no_printing_account_summary_in_main
-    if /printf.+[A|a]ccounts\s+[S|s]ummary/.match program_code
+    if /^[^\/]*printf.+[A|a]ccounts\s+[S|s]ummary/.match program_code
       report.write "A printf for account summary was detectd in your main function"
       report.update_score_by -10
     else
@@ -33,7 +33,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   end
   
   def verify_no_printing_total_number_of_accounts_in_main
-    if /printf.+total number of accounts.+total_number_of_accounts/.match program_code
+    if /^[^\/]*printf.+total number of accounts.+total_number_of_accounts/.match program_code
       report.write "A printf for total_number_of_accounts was detectd in your main function"
       report.update_score_by -10
     else
@@ -51,7 +51,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   end
   
   def verify_no_printing_total_amount_in_accounts_in_main
-    if /printf.+total amount in all accounts.+total_amount_in_accounts/.match program_code
+    if /^[^\/]*printf.+total amount in all accounts.+total_amount_in_accounts/.match program_code
       report.write "A printf for total_amount_in_accounts was detectd in your main function"
       report.update_score_by -10
     else
@@ -69,7 +69,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   end
   
   def verify_no_printing_minimum_balance_fee_percentage_in_main
-    if /printf.+total_minimum_balance_fees.*\/.*total_amount_in_accounts/.match program_code
+    if /^[^\/]*printf.+total_minimum_balance_fees.*\/.*total_amount_in_accounts/.match program_code
       report.write "A printf for 'total_minimum_balance_fees / total_amount_in_accounts' was detectd in your main function"
       report.update_score_by -10
     else
@@ -87,7 +87,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   end
   
   def verify_no_printing_bad_standing_accounts_percentage_in_main
-    if /printf.+total_accounts_with_minimum_balance_fees.*\/.*total_number_of_accounts/.match program_code
+    if /^[^\/]*printf.+total_accounts_with_minimum_balance_fees.*\/.*total_number_of_accounts/.match program_code
       report.write "A printf for 'total_accounts_with_minimum_balance_fees / total_number_of_accounts' was detectd in your main function"
       report.update_score_by -10
     else
