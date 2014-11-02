44daf864246c5b1237458ff23e1fda1e866e7eff
jameyia <jameyia@hawaii.edu>
2014-11-02 02:23:10 -1000

    Create six_one.rb

---
+++ grader/solutions/six_one.rb
@@ -0,0 +1,21 @@
+class Grader::Solution::SixOne < Grader::Solution::Base
+  def analyze_output
+    verify_needs_magic
+  end
+  
+  private
+  
+  def verify_needs magic
+    input = <<-END_OF_INPUT
+7
+    END_OF_INPUT
+    run(input)
+    
+    if /^Your number needs more magic :( $/.match output
+      # great
+    else
+      #report.write "Your program did correctly output \'Your number needs more magic :( \' when given a number less than 100"
+      report.update_score_by -10
+    end
+  end
+end
