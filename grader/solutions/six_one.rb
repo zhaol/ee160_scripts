e5603c768bc796c9c80f1a7bf285bf36ba91c455
jameyia <jameyia@hawaii.edu>
2014-12-10 03:52:58 -1000

    Updated for all three cases

--- grader/solutions/six_one.rb
+++ grader/solutions/six_one.rb
@@ -11,11 +11,40 @@ class Grader::Solution::SixOne < Grader::Solution::Base
     END_OF_INPUT
     run(input)
     
-    if /^Your number needs more magic :\( $/.match output
+    if /^needs magic :\( $/.match output
       # great
     else
-      #report.write "Your program did correctly output \'Your number needs more magic :( \' when given a number less than 100"
+      #report.write "Your program did not correctly output \'Your number needs more magic :( \' when given a number less than 100"
       report.update_score_by -10
     end
   end
+  
+  def verify_very_magical
+    input = <<-END_OF_INPUT
+125
+    END_OF_INPUT
+    run(input)
+    
+    if /^very $/.match output
+      # great
+    else
+      #report.write "Your program did not correctly output \'Your number is very magical! \' when given a number between 100 and 199"
+      report.update_score_by -10
+    end
+  end
+  
+  def verify_needs_magic
+    input = <<-END_OF_INPUT
+233
+    END_OF_INPUT
+    run(input)
+    
+    if /^too $/.match output
+      # great
+    else
+      #report.write "Your program did not correctly output \'Your number is too magical :\( \' when given a number 200 or greater"
+      report.update_score_by -10
+    end
+  end
+  
 end
