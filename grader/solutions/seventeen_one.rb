94701d759f38ac471e242635cbc1fe2b5bf24449
Zhao Li <zhaol@hawaii.edu>
2015-05-01 15:00:03 +0000

    update 17_1

--- grader/solutions/seventeen_one.rb
+++ grader/solutions/seventeen_one.rb
@@ -1,6 +1,6 @@
 class Grader::Solution::SeventeenOne < Grader::Solution::Base
   def analyze_output
-    verify_3
+    verify_3p3
   end
   
   def analyze_syntax
@@ -14,37 +14,37 @@ class Grader::Solution::SeventeenOne < Grader::Solution::Base
   
   private
   
-  def verify_3
+  def verify_3p3
     input = <<-END_OF_INPUT
-3
+3.3
     END_OF_INPUT
     run(input)
 
-    if (/the circumference of the circle is: 18.84/.match output) &&
-      (/the area of the circle is: 28.26/.match output) &&
-      (/the area of the square is: 9.00/.match output)
+    if (/circumference.+20[.]72$/.match output) &&
+      (/area.+circle.+34[.]19$/.match output) &&
+      (/area.+square.+10[.]89$/.match output)
       report.write "The program produced the correct output"
     else
       report_standard_error_message(input, output)
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_pi_macro
-    if /#define\s+PI\s+3.14/.match macros
+    if /#define[\s]+PI[\s]+3.14/.match macros
       report.write "A macro for PI was found"
     else
       report.write "A macro for PI was not found"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_circumference_macro
-    if /#define\s+CIRCUMFERENCE\s+/.match macros
+    if /#define[\s]+CIRCUMFERENCE[\s]+/.match macros
       report.write "A macro for CIRCUMFERENCE was found"
     else
       report.write "A macro for CIRCUMFERENCE was not found"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
 
@@ -53,16 +53,16 @@ class Grader::Solution::SeventeenOne < Grader::Solution::Base
       report.write "A macro for CIRCUMFERENCE was used"
     else
       report.write "A macro for CIRCUMFERENCE was not used"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_circle_area_macro
-    if /#define\s+CIRCLE_AREA\s+/.match macros
+    if /#define[\s]+CIRCLE_AREA[\s]+/.match macros
       report.write "A macro for CIRCLE_AREA was found"
     else
       report.write "A macro for CIRCLE_AREA was not found"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
@@ -71,16 +71,16 @@ class Grader::Solution::SeventeenOne < Grader::Solution::Base
       report.write "A macro for CIRCLE_AREA was used"
     else
       report.write "A macro for CIRCLE_AREA was not used"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_square_area_macro
-    if /#define\s+SQUARE_AREA\s+/.match macros
+    if /#define[\s]+SQUARE_AREA\s+/.match macros
       report.write "A macro for SQUARE_AREA was found"
     else
       report.write "A macro for SQUARE_AREA was not found"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
@@ -89,7 +89,7 @@ class Grader::Solution::SeventeenOne < Grader::Solution::Base
       report.write "A macro for SQUARE_AREA was used"
     else
       report.write "A macro for SQUARE_AREA was not used"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
 end
