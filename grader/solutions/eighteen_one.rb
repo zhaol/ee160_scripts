0180eaaeeda87788dad90c2782a1b5c9b6dcbf29
Zhao Li <zhaol@hawaii.edu>
2015-05-01 14:56:45 +0000

    update lecture 18 assignment

--- grader/solutions/eighteen_one.rb
+++ grader/solutions/eighteen_one.rb
@@ -1,5 +1,10 @@
 class Grader::Solution::EighteenOne < Grader::Solution::Base
+  def analyze_output
+    verify_functionality
+  end
+  
   def analyze_syntax
+    verify_function
     verify_no_printing_account_summary_in_main
     verify_printing_account_summary_in_function
     verify_no_printing_total_number_of_accounts_in_main
@@ -14,10 +19,41 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
   
   private
   
+  def verify_functionality
+    input = <<-END_OF_INPUT
+300
+c
+y
+    END_OF_INPUT
+    run(input)
+   
+    if (/^A minimum balance fee needs to be charged to this account[.]$/.match output) &&
+      (/^Minimum Balance Fee to Account Balance Percentage: 11[.]7%$/.match output) &&
+      (/^The new account balance is [$]265[.]00$/.match output)
+      # great
+    else
+      report_standard_error_message(input, output)
+      report.update_score_by(-10)
+    end
+  end
+  
+  def verify_function
+    if /^void[\s]?display_accounts_summary_information[\s]*[(]
+      [\s]*int[\s]*total_number_of_accounts,
+      [\s]*float[\s]*total_amount_in_accounts,
+      [\s]*float[\s]*total_minimum_balance_fees,
+      [\s]*int[\s]*total_accounts_with_minimum_balance_fees[)][\s]*{/x.match helper_functions
+      #great
+    else
+      report.write 'Could not find function definition'
+      report.update_score_by(-10)
+    end
+  end
+  
   def verify_no_printing_account_summary_in_main
     if /^[^\/]*printf.+[A|a]ccounts\s+[S|s]ummary/.match program_code
       report.write "A printf for account summary was detectd in your main function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     else
       # great
     end
@@ -28,14 +64,14 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
       # great
     else
       report.write "No printf for account summary was detectd in your helper function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_no_printing_total_number_of_accounts_in_main
     if /^[^\/]*printf.+total number of accounts.+total_number_of_accounts/.match program_code
       report.write "A printf for total_number_of_accounts was detectd in your main function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     else
       # great
     end
@@ -46,14 +82,14 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
       # great
     else
       report.write "No printf for total_number_of_accounts was detectd in your helper function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_no_printing_total_amount_in_accounts_in_main
     if /^[^\/]*printf.+total amount in all accounts.+total_amount_in_accounts/.match program_code
       report.write "A printf for total_amount_in_accounts was detectd in your main function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     else
       # great
     end
@@ -64,14 +100,14 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
       # great
     else
       report.write "No printf for total_amount_in_accounts was detectd in your helper function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_no_printing_minimum_balance_fee_percentage_in_main
     if /^[^\/]*printf.+total_minimum_balance_fees.*\/.*total_amount_in_accounts/.match program_code
       report.write "A printf for 'total_minimum_balance_fees / total_amount_in_accounts' was detectd in your main function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     else
       # great
     end
@@ -82,14 +118,14 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
       # great
     else
       report.write "No printf for 'total_minimum_balance_fees / total_amount_in_accounts' was detectd in your helper function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
   
   def verify_no_printing_bad_standing_accounts_percentage_in_main
     if /^[^\/]*printf.+total_accounts_with_minimum_balance_fees.*\/.*total_number_of_accounts/.match program_code
       report.write "A printf for 'total_accounts_with_minimum_balance_fees / total_number_of_accounts' was detectd in your main function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     else
       # great
     end
@@ -100,7 +136,7 @@ class Grader::Solution::EighteenOne < Grader::Solution::Base
       # great
     else
       report.write "No printf for 'total_accounts_with_minimum_balance_fees / total_number_of_accounts' was detectd in your helper function"
-      report.update_score_by -10
+      report.update_score_by(-10)
     end
   end
 end
