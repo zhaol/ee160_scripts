da968c7b0464fee2347c9ed416f4d708e1358278
Zhao Li <zhaol@hawaii.edu>
2015-05-02 09:42:44 +0000

    update 24_1

--- grader/solutions/twentyfour_one.rb
+++ grader/solutions/twentyfour_one.rb
@@ -1,8 +1,6 @@
 class Grader::Solution::TwentyfourOne < Grader::Solution::Base
   def analyze_output_files
     verify_account_holder_output_file
-    #@input_file_url = "https://raw.githubusercontent.com/zhaol/ee160/gh-pages/code/files/23_1.input"
-    #verify_total_number_of_mamas
   end
   
   private
@@ -19,12 +17,11 @@ y
     run(input)
     
     ending_account_balance = starting_account_balance - 35  
-    if /#{ending_account_balance}/.match output_file("#{account_holder_name}.txt") # TODO: remove at the end of semester
-    #if /#{ending_account_balance}/.match output_file("#{account_holder_name}.output") # TODO: uncomment at the end of semester
-      report.write "an output file with the correct ending account balance was found"  
+    if /#{ending_account_balance}/.match output_file("#{account_holder_name}.output")
+      report.write "Good job! An output file with the correct ending account balance was found"  
     else
-      report.write "the output file did not contain the correct ending account balance"
-      report.update_score_by -40
+      report.write "Sorry, but the output file did not contain the correct ending account balance"
+      report.update_score_by(-40)
     end  
   end
 end
