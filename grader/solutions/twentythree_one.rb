ab6aad52553f44e9963df54e6f43bf8e208535ba
Zhao Li <zhaol@hawaii.edu>
2015-10-02 02:05:30 +0000

    use correct test input file

--- grader/solutions/twentythree_one.rb
+++ grader/solutions/twentythree_one.rb
@@ -1,6 +1,6 @@
 class Grader::Solution::TwentythreeOne < Grader::Solution::Base
   def analyze_output
-    @input_file_url = "https://raw.githubusercontent.com/zhaol/ee160/gh-pages/code/files/23_1.input"
+    @input_file_url = "https://raw.githubusercontent.com/zhaol/ee160/gh-pages/code/files/test/23_1.input"
     verify_total_number_of_mamas
   end
   
