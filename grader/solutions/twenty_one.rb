559c0880a9de9b834f8d0011e85cdd9621df722e
Zhao Li <zhaol@hawaii.edu>
2015-10-13 07:52:19 +0000

    change test inputs to be more different than assignment test case; also, revert to testing smallest concept (ie just the sorting portion)

--- grader/solutions/twenty_one.rb
+++ grader/solutions/twenty_one.rb
@@ -9,7 +9,7 @@ class Grader::Solution::TwentyOne < Grader::Solution::Base
 
   def verify_account_balances
     input = <<-END_OF_INPUT
-500
+600
 c
 n
 150
@@ -18,22 +18,16 @@ n
 200
 s
 n
-1
+10
 c
 y
     END_OF_INPUT
     run(input)
-    if /====[\s]Accounts[\s]Summary[\s]====[\s=]*
-        The total number of accounts:[\s]5
-        The total amount in all accounts:[\s][$]1351[.]00
-        The percentage of the total minimum balance fees with respect to the total account balances:[\s]10[.]4%
-        The percentage of accounts that were charged a minimum balance fee:[\s]80[.]0%
-        ====[\s]List[\s]of[\s]Account[\s]Balances[\s]====[\s=]*
-        [\s]*1:[\s]*[$][\s]*1[.]00
-        [\s]*2:[\s]*[$][\s]*100[.]00
-        [\s]*3:[\s]*[$][\s]*350[.]00
-        [\s]*4:[\s]*[$][\s]*400[.]00
-        [\s]*5:[\s]*[$][\s]*500[.]00/x.match output 
+    if /List[\s]of[\s]Account[\s]Balances[\s=]*
+        [\s]*1:[\s]*[$][\s]*10[.]00 
+        [\s]*2:[\s]*[$][\s]*150[.]00 
+        [\s]*3:[\s]*[$][\s]*200[.]00 
+        [\s]*4:[\s]*[$][\s]*600[.]00/x.match output 
       # great
     else
       report_standard_error_message(input, output)
